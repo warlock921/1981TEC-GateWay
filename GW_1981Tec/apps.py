603d267d7e3d86bb9423cf2f2851b81d1a9e451b
warlock921 <caoyu921@163.com>
2017-10-15 22:56:00 +0800

    修改setting.py

---
+++ GW_1981Tec/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Gw1981TecConfig(AppConfig):
+    name = 'GW_1981Tec'
--- GateWay_1981Tec/settings.py
+++ GateWay_1981Tec/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'lm8q#y6dk67x2r6r+-3j#5w!y07n76sncd9_4!m29t=!qsgj&0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.0.0.15','localhost','127.0.0.1']
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'GateWay_1981Tec.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gateway_1981tec',
+        'USER':'root',
+        'PASSWORD':'P@ssword',
+        'HOST':'localhost',
+        'PORT':'',
     }
 }
 
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -118,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'GW_1981Tec/static'), 
+]
